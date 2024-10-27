be737651fd1c57f508636ee1020c55e13b4eabaa
unknown <waelabokhzam@gmail.com>
2024-10-28 00:31:14 +0300

    #3

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ btn.addEventListener("click", function(){
   let input=document.getElementById("input-task");
   let li=document.createElement("li");
   li.innerHTML=input.value;
+  li.title="click to delete";
+  li.addEventListener("dblclick", function(){
+    document.getElementById("ul").removeChild(li);
+  });
+  li.addEventListener("click", function(){
+    input.value=li.innerHTML;
+    document.getElementById("ul").removeChild(li);
+  });
   document.getElementById("ul").appendChild(li);
 });
+function deleteTask(){
+  document.getElementById("ul").removeChild(document.getElementsByTagName("li")[0]);
+}
